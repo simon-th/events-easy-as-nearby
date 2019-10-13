f6035f757a04b2707d72a8995a2f65d0858acfda
shaniapaul <shaniapaul4@gmail.com>
2019-10-13 03:03:39 -0500

    Add event markers

--- client/src/EventMap/EventMap.js
+++ client/src/EventMap/EventMap.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import CurrentLocation from './Geolocation';
 import apiKeys from '../api-keys.json';
+import MyEvents from '../MyEvents/MyEvents'
 
 
 export class MapContainer extends Component {
@@ -33,7 +34,13 @@ export class MapContainer extends Component {
         centerAroundCurrentLocation
         google={this.props.google}
       >
-        <Marker onClick={this.onMarkerClick} name={'current location'} />
+        <Marker position={{lat: 30.286358, lng: -97.7456957}} onClick={this.onMarkerClick} name={'Yash Birthday'} />
+        <Marker position={{lat: 30.2842331, lng: -97.7386967}} onClick={this.onMarkerClick} name={'Wood Chopping Contest'} />
+        <Marker position={{lat: 30.2870417, lng: -97.7461794}} onClick={this.onMarkerClick} name={'Snakes and Ladders Night'} />
+        <Marker position={{lat: 30.2900117, lng: -97.7445804}} onClick={this.onMarkerClick} name={'Smash Tournament'} />
+        <Marker position={{lat: 30.2884957, lng: -97.7376979}} onClick={this.onMarkerClick} name={'IEEE GM #3'} />
+        <Marker position={{lat: 30.2864807, lng: -97.743338}} onClick={this.onMarkerClick} name={'Spongebob Watch Party'} />
+        <Marker position={{lat: 30.2834028, lng: -97.7412837}} onClick={this.onMarkerClick} name={'Gong Cha Profit Share'} />
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
