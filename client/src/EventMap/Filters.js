0ac64dbea68ec12424f3285c9d4a589c894c8064
angellynncheng <pink1440@gmail.com>
2019-10-23 03:49:55 -0500

    Fixing some errors from pulling that prevents rendering

--- client/src/EventMap/Filters.js
+++ client/src/EventMap/Filters.js
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import { sizing } from '@material-ui/system';
 import FilterObject from '../EventMap/FilterObject'
-import { AutoComplete } from 'material-ui';
 
 const drawerWidth = 240;
 
@@ -47,7 +46,6 @@ export default function PermanentDrawerRight() {
           paper: classes.drawerPaper,
         }}
         anchor="right"
-        
       >
         <div className={classes.toolbar} />
         <Divider />
