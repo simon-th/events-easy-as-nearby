e0e75993d485fbb1796245f8050a356ed6b719ad
simon-th <simon.thoque@gmail.com>
2019-10-25 15:38:56 -0500

    Implement backend filtering

--- server/routes/Events/events.js
+++ server/routes/Events/events.js
@@ -1,56 +1,91 @@
 const express = require('express');
-const mongoose = require('mongoose');
-var cors = require('cors');
+const geolib = require('geolib');
+const convert = require('convert-units');
 const Category = require('../../mongodb_schemas/Category');
 const Event = require('../../mongodb_schemas/Event');
 
-
 const router = express.Router();
 
-const DATABASE_NAME = 'events';
-const CONNECTION_URL = `mongodb+srv://huy0123:huy_utexas@explocationdb-qtiwe.gcp.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true});
-mongoose.Promise = global.Promise;
-
-
-var db = mongoose.connection;
-db.once('open', () => {
-  dbConnected = true;
-  console.log('connected to database');
-});
-db.on('error', () => {
-  console.error.bind(console, 'Mongo connection error');
-});
+async function filterEventsByDistance(events, source, distance) {
+  var filteredEvents = [];
+  events.forEach((e) => {
+    var d = geolib.getDistance(source, {
+      latitude: e.latitude,
+      longitude: e.longitude
+    });
+    d = convert(d).from('m').to('mi');
+    if (d <= distance) {
+      filteredEvents.push(e);
+    }
+  });
+  return filteredEvents;
+}
 
 router.get('/test', (req, res) => {
   res.send('events api route works!');
 });
 
-router.get('/category', (req, res) => {
-  Category.find({ id: req.query.id }, (err, data) => {
-    if (err) return res.json({ success: false, error: err});
-    return res.json({ success: true, data: data});
-  });
-});
-
-// axios.get('/filter/?f1=104&f2=107&f3=120')
-
-router.get('/filter', (req, res) => {
-  var events = []
-  // for (var i = 1; i < 21; i++) {
-  var i = 1;
-  var key = `f${i}`;
-  if (req.query[key]) {
-    console.log(req.query[key]);
-    Event.find({ category_id: req.query[key] }, (err, data) => {
-      if (err) return res.json({ success: false, error: err});
-      return res.json({ success: true, data: data});
+router.get('/category', async (req, res) => {
+  await Category.find({ id: req.query.id})
+    .exec()
+    .then(data => {
+      // console.log(data);
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({message: 'aiya'})
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
     });
+  });
+
+router.get('/filter', async (req, res) => {
+  try {
+    var events = [];
+    var isFilter = false;
+    for (var i = 1; i < 21; i++) {
+    // var i = 1;
+      var key = `f${i}`;
+      if (req.query[key]) {
+        isFilter = true;
+        const id = req.query[key];
+        await Event.find({ 'category_id': id})
+        .exec()
+        .then(data => {
+          if (data) events = events.concat(data);
+          else res.status(404).json({message: 'aiya'});
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({error: err});
+        });
+      } else {
+        if (!isFilter) {
+          await Event.find()
+          .exec()
+          .then(data => {
+            if (data) events = data;
+            else res.status(404).json({message: 'aiya'});
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+          });
+        }
+        break;
+      }
+    }
+    const distance = req.query.distance;
+    const latitude = req.query.latitude;
+    const longitude = req.query.longitude;
+    var filteredEvents = await filterEventsByDistance(events, {latitude: latitude, longitude: longitude}, distance);
+    res.status(200).json(filteredEvents);
+  } catch(error) {
+    console.log(error);
   }
-    // console.log(filters[i]);
-  // }
-  // res.send(events);
 });
 
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,21 @@ var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
+const DATABASE_NAME = 'events';
+const CONNECTION_URL = `mongodb+srv://huy0123:huy_utexas@explocationdb-qtiwe.gcp.mongodb.net/${DATABASE_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+db.once('open', () => {
+  dbConnected = true;
+  console.log('connected to database from server.js');
+});
+db.on('error', () => {
+  console.error.bind(console, 'Mongo connection error');
+});
+
 const API_PORT = 3001;
 const app = express();
 app.use(cors());
