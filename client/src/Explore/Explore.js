b8a7a3fbdb091c5b03d352361bb93cec82abdcde
angellynncheng <pink1440@gmail.com>
2019-10-25 21:26:51 -0500

    new Explore page

--- client/src/Explore/Explore.js
+++ client/src/Explore/Explore.js
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
-import {
-  Card, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Row, Col
-} from "reactstrap";
-//import axios from "axios";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import "./Explore.css";
 
 
 class Explore extends Component {
-
+  
   state = {
     data: [],
     name: null,
@@ -34,25 +38,40 @@ class Explore extends Component {
           <h2>Events Happening Nearby</h2>
         </div>
         <div>
-            <ul>
+          <Grid container className="grid" spacing={2}>
                 {data.length <= 0
                   ? ''
                   : data.map((dat) => (
-                    <Row>
-                      <Col sm={{ size: 8, offset: 1 }}>
-                          <Card>
-                            <CardBody>
-                              <CardTitle>{dat.name}</CardTitle>
-                              <CardSubtitle>{dat.summary}</CardSubtitle>
-                              <CardText className="text-left">{dat.description}</CardText>
-                              <Button color="success" className="float-right">Save</Button>
-                            </CardBody>
-                          </Card>
-                      </Col>
-                    </Row>
+                    <Card className="exploreCard">
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          alt="No image available"
+                          height="180"
+                          image="https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg"
+                          title={dat.name}
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            {dat.name}
+                          </Typography>
+                          <Typography variant="body2" color="textSecondary" component="p">
+                          {dat.summary}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions className="buttons">
+                        <Button size="small" color="primary">
+                          Save Event
+                        </Button>
+                        <Button size="small" color="primary">
+                          Learn More
+                        </Button>
+                      </CardActions>
+                    </Card>
                   ))}
                 <br />
-            </ul>
+            </Grid>
         </div>
       </div>
     );
